55101463bf0e1d2b8bc1112fc2bc59af8adf6bbb
Santiago Guzman <guzmansantiago833@gmail.com>
2025-09-06 22:48:01 -0500

    6 Septiembre 2025

--- Migrando a Parcel/src/vite-env.d.ts
+++ Migrando a Parcel/src/vite-env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="vite/client" />
+declare module "bundle-text:*" {
+    const value: string;
+    export default value;
+}
+
+declare module "data-url:*" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
